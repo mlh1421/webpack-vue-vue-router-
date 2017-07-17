cb5ade440258e57c4aca01f22d4b99af5e86d8df
mlh1421 <1421590493@qq.com>
2017-07-17 16:19:11 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+    <div id="app">
+        <div class="header">
+            <a href="#" v-on:click='greet'>menu</a>
+            <router-link to='/' >首页</router-link>
+            <router-link to='/b' >bbb</router-link>
+            <router-link to='/c' >ccc</router-link>
+        </div>
+        <div class="left">
+        </div>
+        <div class="body">
+            <router-view></router-view>
+        </div>
+        <!-- <my :s='stories'></my> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data:function () {
+        return {
+            date:'20170717'
+        }
+    },
+    components:{
+        // my:my
+    },
+    methods:{
+        greet:function () {
+            var obj=document.getElementsByClassName('left')[0];
+            var left =obj.style.left;
+            var width=window.innerWidth;
+            left=width*(-0.7)+'px';
+            // alert(width);
+            console.log(left);
+            //left=(left==0)?(-0.7) : 0;
+        }
+    }
+}
+</script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+}
+body{
+    position: relative;
+}
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /*text-align: center;*/
+    color: #2c3e50;
+    /*position: relative;*/
+}
+.left{
+    position: absolute;
+    background: green;
+    /*left:-70%;*/
+    /*left:0;*/
+    /*width: 70%;*/
+    /*height: 100%;*/
+}
+h1{
+    background: #eee;
+    color:#555;
+
+}
+.header{
+    position: fixed;
+    text-align: center;
+    top:0;
+    width: 100%;
+    z-index: 10;
+}
+.body{
+    margin-top: 50px;
+}
+.header>a{
+    width:calc(100%/4);
+    box-sizing: border-box;
+    border: 1px solid green;
+    height: 50px;
+    line-height: 50px;
+    background: #eee;
+    /*border:1px solid red;*/
+    display:block;
+    float: left;
+    text-decoration: none;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <h1>今日热闻</h1>
+        <ul class="aa">
+            <li v-for='i in stories'>
+                <!-- <a :href="''+i.id"> {{i.title}} </a> -->
+                <router-link :to="{path:'/content',query:{href:'https://zhihu-daily.leanapp.cn/api/v1/contents/'+i.id}}">{{i.title}}</router-link>
+                <!-- <router-view></router-view> -->
+                <img :src="'//images.weserv.nl/?url='+i.images[0].replace('https://','')" alt="#">
+            </li>
+        </ul>
+        <!-- <router-link to='/a/d'>ddd</router-link> -->
+        <!-- <router-view></router-view> -->
+        <!-- <my :s='data'></my> -->
+    </div>
+</template>
+
+<script>
+var xhr=new XMLHttpRequest();
+xhr.open("get","http://mlh1421.cn/1.php",false);
+xhr.send(null);
+var data=xhr.responseText;
+data=JSON.parse(data);
+console.log(data);
+var stories=data.STORIES.stories;
+var date=data.STORIES.date;
+export default {
+    name: 'hello',
+    // props:['s'],
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App',
+            stories:stories
+        }
+    }
+}
+</script>
+
+<style scoped>
+.aa{
+}
+h1, h2 {
+    font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    /*display: inline-block;*/
+    /*line-height: 200px;*/
+    border: 1px solid #eee;
+    box-shadow: 0 3px 5px #eee;
+    margin: 10px 10px;
+}
+
+li a {
+    color: #555;
+    width: 60%;
+    vertical-align: middle;
+    display: inline-block;
+    box-sizing: border-box;
+    padding: 0 10px;
+    text-decoration: none;
+}
+li img{
+    width: 35%;
+    display: inline-block;
+    vertical-align: middle;
+}
+</style>
